9e7537d3997a1b2ffb957c6e4377d15a1350b551
Maxim Cherniy <mch.maximcherniy@gmail.com>
2021-02-26 16:50:52 +0200

    init: start "Mailchimp" task

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+let mode = "development";
+let target = "web";
+
+if (process.env.NODE_ENV === "production") { 
+  mode = "production";
+  target = "browserslist";
+}
+
+module.exports = {
+  mode: mode,
+  target: target,
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[name][ext][query]",
+    // assetModuleFilename: "images/[hash][ext][query]",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
+        type: "asset"
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
+          "css-loader", 
+          "postcss-loader", 
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "/src/index.html")
+    })
+  ],
+
+  performance: {
+    hints: false,
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  }
+};
